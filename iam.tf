b1a3689fada61c0a679f65ca1f686c78cc3021f4
amanda-campos <amandacamposx2@gmail.com>
2025-04-18 18:45:30 -0400

    feat(iam):configura role para ec2

---
+++ iam.tf
@@ -0,0 +1,57 @@
+# iam.tf
+
+# IAM Role para EC2
+resource "aws_iam_role" "ec2_role" {
+  name = "sprint7-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# adicone aqui as policy
+# Ex de policy para acesso ao S3, Bedrock e CloudWatch
+resource "aws_iam_role_policy" "inline_policy" {
+  name = "nome_teste"
+  role = aws_iam_role.ec2_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:GetObject", "s3:PutObject"]
+        Resource = "arn:aws:s3:::${var.bucket_name}/*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["bedrock:*"]
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Instance Profile para associar à EC2
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "nome/teste"
+  role = aws_iam_role.ec2_role.name
+}
